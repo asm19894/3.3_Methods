7170ede2aa8c73fce42bb99857d7542e5666f9e5
Anna Smirnova <sanna_i@mail.ru>
2023-04-03 13:48:31 +0300

    26,27 Задача

---
+++ 3.3 Семинар/026_task/Program.cs
@@ -0,0 +1,24 @@
+﻿// 26 Напишите программу,
+//  которая принимает на вход число и выдаёт количество цифр в числе.
+
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+
+
+int CountNum(int n)
+{
+    int count = 0;
+
+    while (n > 0)
+    {
+        n /= 10;
+        count++;
+    }
+    return count;
+}
+
+Console.Write("Введите число: ");
+int s = int.Parse(Console.ReadLine());
+Console.WriteLine($"Количество цифр = {CountNum(s)}");
---
+++ 3.3 Семинар/027_task/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4-> 24
+//5-> 120 == 1 * 2 * 3 * 4 * 5
+
+
+int MoulNumber (int n) {
+    int moul = 1;
+    for (int i = 1; i <= n; i++)
+        moul = moul * i;
+    return moul;
+}
+
+Console.WriteLine("Введите целое число: ");
+int s = int.Parse(Console.ReadLine());
+Console.WriteLine($"Произведение чисел равно {MoulNumber(s)}");
